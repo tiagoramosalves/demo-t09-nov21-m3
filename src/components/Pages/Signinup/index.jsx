551eb5cefde30bb8f859253a4d22eb397479025e
Tiago Ramos Alves <tiagoramosalves@hotmail.com>
2022-04-14 10:12:57 -0300

    update: fomulario com material UI

--- src/components/Pages/Signinup/index.jsx
+++ src/components/Pages/Signinup/index.jsx
@@ -7,6 +7,16 @@ import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
+import {
+  Avatar,
+  Typography,
+  Container,
+  TextField,
+  Button,
+} from "@mui/material";
+import { PersonAdd } from "@mui/icons-material";
+import { Box } from "@mui/system";
+
 const Siginup = () => {
   const history = useHistory();
 
@@ -46,17 +56,82 @@ const Siginup = () => {
   return (
     <>
       <MenuBar />
-      <h1>Siginup</h1>
+
+      <Container component="main">
+        <Box
+          sx={{
+            marginTop: 10,
+            display: "flex",
+            flexDirection: "Column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: "1", bgcolor:"secondary.main" }}>
+            <PersonAdd />
+          </Avatar>
+          <Typography variant="h4">Cadastro de Usuário</Typography>
+          <Box
+            onSubmit={handleSubmit(onRegister)}
+            component="form"
+            sx={{ mt: 1, width: "40%" }}
+          >
+            <TextField
+              {...register("username")}
+              margin="normal"
+              fullWidth
+              label="Nome"
+              helperText={errors.username?.message}
+              error={!!errors.username?.message}
+            />
+
+            <TextField
+              {...register("email")}
+              margin="normal"
+              fullWidth
+              label="Email"
+              helperText={errors.email?.message}
+              error={!!errors.email?.message}
+            />
+
+            <TextField
+              {...register("password")}
+              margin="normal"
+              fullWidth
+              label="Senha"
+              helperText={errors.password?.message}
+              error={!!errors.password?.message}
+            />
+
+            <TextField
+              {...register("confirmPassword")}
+              margin="normal"
+              fullWidth
+              label="Conforme Senha"
+              helperText={errors.confirmPassword?.message}
+              error={!!errors.confirmPassword?.message}
+            />
+
+            <Button fullWidth type="submit" variant="contained">
+              Cadastrar
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+
+      {/* <h1>Siginup</h1>
       <button onClick={() => history.push("/")}>Sair</button>
       <FormStyled onSubmit={handleSubmit(onRegister)}>
+
         <label>Username:</label>
+
         <input type="text" {...register("username")} />
-        {/* {<span>{errors.username?.message}</span>} */}
+        {<span>{errors.username?.message}</span>}
         {errors.username && (
           <span className="error">{errors.username.message}</span>
         )}
 
         <label>Email:</label>
+
         <input type="text" {...register("email")} />
         {errors.email && <span className="error">{errors.email.message}</span>}
 
@@ -79,7 +154,7 @@ const Siginup = () => {
         {errors.terms && <span className="error">{errors.terms.message}</span>}
 
         <button type="submit">Create an Account</button>
-      </FormStyled>
+      </FormStyled> */}
     </>
   );
 };
